6dd44a60680c763914840ed2589e626ee58d0167
Nursultan Khavazov <nursultan311204@icloud.com>
2024-07-07 15:24:31 +0600

    redux-toolkit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import Navigate from './Navigate/Navigate';
 import News from './News/News';
+import Popular from './Popular/Popular';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner/>
             <Navigate/>
             <News/>
+            <Popular/>
         </div>
     );
 };
--- src/Pages/Home/Popular/Popular.jsx
+++ src/Pages/Home/Popular/Popular.jsx
@@ -5,17 +5,39 @@ import { useSelector } from 'react-redux';
 
 
 const Popular = () => {
-    const {data} = useSelector(state=>state.getAllSets)
-
-
+    const { data } = useSelector(state => state.setsSlice)
+    const limitedData = data.slice(0, 6);
+    const keysSet = new Set();
     return (
         <section className='popular'>
             <div className="container">
                 <h2>Популярные наборы</h2>
-                
+                <div className="popular__cards">
+                    {limitedData.map((el, idx) => {
+                        let key = el.id ? el.id : `key-${idx}`;
+                        while (keysSet.has(key)) {
+                            key = `key-${idx}-${Math.random()}`;
+                        }
+                        keysSet.add(key);
+
+                        return (
+                            <div className="popular__card" key={key}>
+                                <img src={el.img} alt={el.name} />
+                                <h4>{el.name}</h4>
+                                <p>{el.compound}</p>
+                                <div className="popular__price">
+                                    <p className="original-price">{el.price} руб</p>
+                                    <p className="discounted-price">{(el.price - (el.price / 100 * el.sale)).toFixed(2)} руб</p>
+                                </div>
+                                <button>В корзину</button>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </section>
     );
-};
 
+
+};
 export default Popular;
